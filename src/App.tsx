cebae62da053864d47028f54e4b68b39b5531e4a
PietyE <kvasovdim17@gmail.com>
2022-04-15 22:13:38 +0300

    created task title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemeProvider, Box } from '@mui/material';
 import { theme } from './theme/theme';
-import './App.scss';
 import { Exercise } from './components/Exercise';
+import './App.scss';
 
 const App = () => (
   <ThemeProvider theme={theme}>
--- src/components/Exercise/Exercise.tsx
+++ src/components/Exercise/Exercise.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import { Header } from './components/Header';
+import { Task } from './components/Task';
 
 export const Exercise = () => (
   <Container maxWidth="xl" sx={{ p: 5 }}>
     <Header />
+    <Task />
   </Container>
 );
