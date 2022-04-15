b73e1237b8f8304b8a2353b39310c615626978fe
PietyE <kvasovdim17@gmail.com>
2022-04-16 02:24:14 +0300

    ready w/o mob

--- src/components/Exercise/components/Task/Task.tsx
+++ src/components/Exercise/components/Task/Task.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-
+import { Sentence } from './components/Sentence';
+import { exerciseWords } from '../../../../data/exercicesWords';
 import './Task.scss';
 
 export const Task = () => (
@@ -10,6 +11,11 @@ export const Task = () => (
         <span className="key-word-example">Przykład:</span> Książka jest{' '}
         <span className="key-word">lepsza</span> niż film
       </Typography>
+      {exerciseWords.map((sentence, index) => (
+        <Sentence key={index} keyWord={sentence.keyWord}>
+          {sentence.sentence}
+        </Sentence>
+      ))}
     </Box>
   </main>
 );
